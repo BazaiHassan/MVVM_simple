b173bc40317e683899112216a4282ec3ec640a5d
HBazai <bazaee.hassan@gmail.com>
2022-03-24 16:25:54 +0430

    Final Version of best practice

--- app/src/main/java/com/masterdev/mvvm_simple/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/masterdev/mvvm_simple/viewmodel/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.masterdev.mvvm_simple.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.masterdev.mvvm_simple.model.Todo
@@ -8,19 +9,21 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainViewModel:ViewModel() {
+class MainViewModel : ViewModel() {
 
     private val repository = Repository()
 
-    val todosLiveData = MutableLiveData<ArrayList<Todo>>()
+    private val _todosLiveData = MutableLiveData<ArrayList<Todo>>()
 
-    fun getTodos(){
-        repository.getTodos?.enqueue(object : Callback<ArrayList<Todo>>{
+    val todosLiveData: LiveData<ArrayList<Todo>> get() = _todosLiveData
+
+    fun getTodos() {
+        repository.getTodos?.enqueue(object : Callback<ArrayList<Todo>> {
             override fun onResponse(
                 call: Call<ArrayList<Todo>>,
                 response: Response<ArrayList<Todo>>
             ) {
-                todosLiveData.postValue(response.body())
+                _todosLiveData.postValue(response.body())
             }
 
             override fun onFailure(call: Call<ArrayList<Todo>>, t: Throwable) {
